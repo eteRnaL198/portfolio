8a6a6449b77e2ded9956c0506c7ec808a3f64450
eternal198 <ikuya313198@gmail.com>
2021-06-10 14:27:39 +0900

    first commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
+const Layout = ({ children, title }: Props) => (
   <div>
     <Head>
       <title>{title}</title>
@@ -18,22 +18,22 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <nav>
         <Link href="/">
           <a>Home</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
+        <Link href="/skills">
+          <a>Skills</a>
+        </Link>
         <Link href="/about">
           <a>About</a>
-        </Link>{' '}
-        |{' '}
+        </Link>
         <Link href="/users">
           <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
+        </Link>
+        <a href="/api/users">Users API</a>
       </nav>
     </header>
     {children}
     <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
+      <small>&copy; 2021 eteRnaL198</small>
     </footer>
   </div>
 )
---
+++ src/pages/skills.tsx
@@ -0,0 +1,9 @@
+import { NextPage } from 'next'
+
+const SkillsPage: NextPage = () => {
+  return (
+    <p>html</p>
+  )
+}
+
+export default SkillsPage
