bd05247cb5f91a71750a4ea0e2a5a4494e8a75cd
eternal198 <ikuya313198@gmail.com>
2021-06-18 09:04:06 +0900

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -7,43 +7,40 @@ type Props = {
   title?: string
 }
 
-const Layout = ({ children, title }: Props) => (
-  <div className="flex flex-col min-h-full bg-blue-100">
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" href="/f_f_business_6_svg_f_business_6_1nbg.svg" />
-    </Head>
-    <header className="bg-white py-4 rounded-b-2xl shadow-md sticky top-0 w-full">
-      <nav className="flex font-bold justify-between mx-auto text-2xl w-11/12">
-        <Link href="/">
-          <a className=" hover:text-gray-500"
-          >Portfolio</a>
-        </Link>
-        <div className="flex justify-between">
-          <Link href="/profile">
-            <a className="ml-10 hover:text-gray-500"
-            >Profile</a>
-          </Link>
-          <Link href="/skills">
-            <a className="ml-10 hover:text-gray-500"
-            >Skills</a>
-          </Link>
-          <Link href="/works">
-            <a className="ml-10 hover:text-gray-500"
-            >Works</a>
-          </Link>
-        </div>
-      </nav>
-    </header>
-    <main className="flex flex-1">
-      {children}
-    </main>
-    <footer className="bg-gray-800 flex justify-center">
-      <small className="p-1 text-white text-xs">&copy; 2021 Ikuya Nagaki</small>
-    </footer>
-  </div>
-)
+const Layout = ({ children, title }: Props) => {
+  const navs = [
+    { name: "Profile", link: "/"},
+    { name: "Portfolio", link: "/portfolio"},
+    { name: "Skills", link: "/skills"},
+    { name: "Works", link: "/works"},
+  ];
+
+  return (
+    <div className="flex flex-col min-h-full bg-blue-100">
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="icon" href="/f_f_business_6_svg_f_business_6_1nbg.svg" />
+      </Head>
+      <header className="bg-white py-4 rounded-b-2xl shadow-md sticky top-0 w-full">
+        <nav className="flex font-bold justify-between mx-auto text-2xl w-11/12">
+          {navs.map((nav, idx) => (
+            <Link href={nav.link} key={idx}>
+              <a className=" hover:text-gray-500"
+              >{nav.name}</a>
+            </Link>
+          ))}
+        </nav>
+      </header>
+      <main className="flex flex-1">
+        {children}
+      </main>
+      <footer className="bg-gray-800 flex justify-center">
+        <small className="p-1 text-white text-xs">&copy; 2021 Ikuya Nagaki</small>
+      </footer>
+    </div>
+  )
+}
 
 export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,17 @@ import Layout from '../components/Layout'
 
 const IndexPage = () => (
   <Layout title="Portfolio | Home">
-    <div className="flex mx-auto w-11/12">
-      <div className="flex flex-col h-full items-center justify-center leading-loose tracking-wide text-3xl w-full">
-        <p className=""
-        >Welcome to my portfolio!</p>
-        <p className=""
-        >Please see in order from my profile</p>
+    <div className="flex flex-col mx-auto w-11/12 md:flex-row">
+      {/* <div className="flex flex-col flex-grow items-center justify-center leading-loose tracking-wide text-xl w-full sm:text-3xl"> */}
+      <div className="flex flex-col items-center justify-center leading-loose tracking-wide text-xl w-full sm:text-3xl">
+        <p>Welcome to my portfolio!</p>
+        <p>Please see in order from my profile</p>
         <Link href="/profile">
-        <a className="bg-blue-400 mt-4 px-8 rounded-2xl shadow-md text-white hover:bg-blue-300 hover:shadow-sm focus:bg-blue-300 focus:shadow-sm"
+          <a className="bg-blue-400 mt-4 px-8 rounded-2xl shadow-md text-white hover:bg-blue-300 hover:shadow-sm focus:bg-blue-300 focus:shadow-sm"
           >Profile</a>
         </Link>
       </div>
-      <img src="/images/undraw_Hello_re_3evm.svg" width="350" alt="" />
+      <img src="/images/undraw_Hello_re_3evm.svg" alt="" className="h-40 mb-10" />
     </div>
   </Layout>
 )
