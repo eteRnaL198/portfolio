c04681435d22a6b71866a40ff92f5a46b25a2bf7
Ikuya Nagaki <ikuya313198@gmail.com>
2024-04-04 21:15:57 +0900

    update

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,37 +1,53 @@
-import { NextPage } from 'next'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
+import Layout from "../components/Layout";
 
 const Profile: NextPage = () => {
   return (
     <Layout title="Portfolio | Profile">
       <div className="mx-auto py-7 w-11/12 max-w-screen-lg">
-        <h1 className="font-bold text-4xl text-center tracking-wider mb-7"
-        >Profile</h1>
+        <h1 className="font-bold text-4xl text-center tracking-wider mb-7">
+          Profile
+        </h1>
         <div className="grid gap-10 sm:grid-cols-2">
           <div className="bg-white grid gap-4 p-8 rounded-3xl tracking-wide shadow-md">
             <div>
               <p className="text-xl sm:text-2xl">永木 郁也</p>
-              <p className="sm:text-xl">都内私立大学 情報工学部 2年</p>
+              <p className="sm:text-xl">東京都市大学 情報工学部 卒業</p>
+              <p className="sm:text-xl">東京工業大学 情報理工学院 修士1年</p>
             </div>
             <div className="sm:text-lg">
-              <p>大学でコンピュータサイエンスを学びながら、web系の言語を独学しています。</p>
-              <p>2020年4月から一年間、主にフロントエンドを独学してきました。</p>
-              <p>将来はwebエンジニアを目指しています。</p>
+              <p>
+                2021年11月からかっこ株式会社でエンジニアとしてインターンをしており､バックエンドからフロントエンドまで開発に携わっています｡
+              </p>
+              <p>
+                学士の卒業研究では､C言語をベースにしたアスペクト指向言語の言語処理系を開発しました｡
+              </p>
+              <p>現在､修士の卒業研究で開発支援ツールを開発しています｡</p>
             </div>
             <div className="flex">
-              <a href="https://twitter.com/programikuya" className="hover:opacity-70 mr-4">
+              <a
+                href="https://twitter.com/programikuya"
+                className="hover:opacity-70 mr-4"
+              >
                 <img src="/images/twitter.svg" alt="" className="h-9" />
               </a>
-              <a href="https://github.com/eteRnaL198" className="hover:opacity-70">
+              <a
+                href="https://github.com/eteRnaL198"
+                className="hover:opacity-70"
+              >
                 <img src="/images/github-icon.svg" alt="" className="h-9" />
               </a>
             </div>
           </div>
-          <img src="/images/undraw_feeling_proud_qne1.svg" alt="" className="w-full" />
+          <img
+            src="/images/undraw_feeling_proud_qne1.svg"
+            alt=""
+            className="w-full"
+          />
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
--- src/pages/works.tsx
+++ src/pages/works.tsx
@@ -1,71 +1,96 @@
-import { NextPage } from 'next'
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
+import Link from "next/link";
+import Layout from "../components/Layout";
 
 const Works: NextPage = () => {
   return (
     <Layout title="Portfolio | Works">
       <div className="mx-auto py-7 w-11/12 max-w-screen-lg">
-        <h1 className="font-bold text-4xl text-center tracking-wider mb-7"
-        >Works</h1>
+        <h1 className="font-bold text-4xl text-center tracking-wider mb-7">
+          Works
+        </h1>
         <div className="grid gap-10">
           <div className="grid gap-5 items-center sm:grid-cols-2">
-            <img src="/images/wordchaintalker.jpg" alt="" className="shadow-md w-3/5 mx-auto"/>
+            <img
+              src="/images/wordchaintalker.jpg"
+              alt=""
+              className="shadow-md w-3/5 mx-auto"
+            />
             <div className="bg-white flex flex-col flex-grow p-10 rounded-3xl shadow-md w-full">
-              <h2 className="font-bold mb-1 text-xl tracking-wider sm:text-2xl"
-              >Word Chain Talker</h2>
+              <h2 className="font-bold mb-1 text-xl tracking-wider sm:text-2xl">
+                Word Chain Talker
+              </h2>
               <div className="text-lg sm:text-lg">
                 <p>ボットと尻取りができるアプリです。</p>
-                <p>名前によってユーザーを管理しているため、名前を入力するだけでゲームを始められます。</p>
+                <p>
+                  名前によってユーザーを管理しているため、名前を入力するだけでゲームを始められます。
+                </p>
                 <p>ランキング機能を実装しました。</p>
-                <p>使用した技術は以下になります。</p>
-                <p className="font-bold tracking-wide"
-                >React, TypeScript, TailwindCSS, Firebase</p>
+                <p>使用した技術は以下です。</p>
+                <p className="font-bold tracking-wide">
+                  React, TypeScript, TailwindCSS, Firebase
+                </p>
               </div>
               <div className="flex flex-grow items-end justify-center mt-3 w-full">
-                <a href="https://wordchaingame-3e0fc.web.app/" className="bg-blue-400 font-bold leading-normal px-8 rounded-2xl shadow-md tracking-wider text-2xl text-white hover:bg-blue-300 hover:shadow-sm focus:bg-blue-300 focus:shadow-sm"
-                >VISIT</a>
+                <a
+                  href="https://wordchaingame-3e0fc.web.app/"
+                  className="bg-blue-400 font-bold leading-normal px-8 rounded-2xl shadow-md tracking-wider text-2xl text-white hover:bg-blue-300 hover:shadow-sm focus:bg-blue-300 focus:shadow-sm"
+                >
+                  VISIT
+                </a>
               </div>
             </div>
           </div>
           <div className="grid gap-5 items-center sm:grid-cols-2">
-            <img src="/images/portfolio.png" alt="" className="shadow-md"/>
+            <img src="/images/portfolio.png" alt="" className="shadow-md" />
             <div className="bg-white flex flex-col flex-grow p-10 rounded-3xl shadow-md w-full">
-              <h2 className="font-bold mb-1 text-2xl tracking-wider"
-              >Portfolio</h2>
+              <h2 className="font-bold mb-1 text-2xl tracking-wider">
+                Portfolio
+              </h2>
               <div className="text-lg">
                 <p>現在ご覧になっているサイトです。</p>
                 <p>レスポンシブ対応しております。</p>
-                <p>使用した技術は以下になります。</p>
-                <p className="font-bold tracking-wide"
-                >Next.js, TypeScript, TailwindCSS</p>
+                <p>使用した技術は以下です。</p>
+                <p className="font-bold tracking-wide">
+                  Next.js, TypeScript, TailwindCSS
+                </p>
               </div>
               <div className="flex flex-grow items-end justify-center mt-3 w-full">
                 <Link href="/">
-                  <a className="bg-blue-400 font-bold leading-normal px-8 rounded-2xl shadow-md tracking-wider text-2xl text-white hover:bg-blue-300 hover:shadow-sm focus:bg-blue-300 focus:shadow-sm"
-                  >VISIT</a>
+                  <a className="bg-blue-400 font-bold leading-normal px-8 rounded-2xl shadow-md tracking-wider text-2xl text-white hover:bg-blue-300 hover:shadow-sm focus:bg-blue-300 focus:shadow-sm">
+                    VISIT
+                  </a>
                 </Link>
               </div>
             </div>
           </div>
           <div className="grid gap-5 items-center sm:grid-cols-2">
-            <img src="/images/devhall.png" alt="" className="shadow-md"/>
+            <img src="/images/comeel.jpg" alt="" className="shadow-md" />
             <div className="bg-white flex flex-col flex-grow p-10 rounded-3xl shadow-md w-full">
-              <h2 className="font-bold mb-1 text-2xl tracking-wider"
-              >Dev Hall</h2>
+              <h2 className="font-bold mb-1 text-2xl tracking-wider">
+                こみ〜る
+              </h2>
               <div className="text-lg">
-                <p>駆け出しエンジニア同士で共同開発をする時の手助けをするアプリです。</p>
+                <p>こども食堂の支援を促すサービスです｡</p>
+                <p>使用した技術は以下です。</p>
+                <p className="font-bold tracking-wide">
+                  React, TypeScript, TailwindCSS, Firebase
+                </p>
               </div>
               <div className="flex flex-grow items-end justify-center mt-3 w-full">
-                <p className="bg-gray-400 font-bold leading-normal px-8 rounded-2xl shadow-md text-center text-2xl text-white "
-                >Under Development...</p>
+                <a
+                  href="https://comeel.web.app/"
+                  className="bg-blue-400 font-bold leading-normal px-8 rounded-2xl shadow-md text-center text-2xl text-white "
+                >
+                  VISIT
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default Works
+export default Works;
