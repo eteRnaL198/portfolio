ef248d2aa081261722ab67c48ac61b20bc35137e
Ikuya Nagaki <ikuya313198@gmail.com>
2024-12-06 00:39:13 +0900

    update

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,57 +1,87 @@
-import { NextPage } from 'next'
-import Layout from '../components/Layout'
+import { NextPage } from "next";
+import Layout from "../components/Layout";
 
 const SkillsPage: NextPage = () => {
   return (
     <Layout title="Portfolio | Skills">
       <div className="mx-auto py-7 w-11/12 max-w-screen-lg">
-        <h1 className="font-bold text-4xl text-center tracking-wider mb-7"
-        >Skills</h1>
-        <div className="grid grid-cols-2 gap-8 text-xl tracking-wider sm:grid-cols-3">
+        <h1 className="font-bold text-4xl text-center tracking-wider">
+          Skills
+        </h1>
+        <h2 className="font-bold text-2xl text-center tracking-wider mt-7">
+          実務経験あり
+        </h2>
+        <div className="grid grid-cols-2 gap-8 text-xl tracking-wider mt-7 sm:grid-cols-3">
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/html-5.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >HTML</p>
+            <p className="text-center text-bold">HTML</p>
           </div>
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/css-3.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >CSS</p>
+            <p className="text-center text-bold">CSS</p>
           </div>
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/sass.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >Sass</p>
+            <p className="text-center text-bold">JavaScript</p>
           </div>
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/javascript.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >JavaScript</p>
+            <p className="text-center text-bold">TypeScript</p>
           </div>
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/typescript-icon.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >TypeScript</p>
+            <p className="text-center text-bold">React</p>
           </div>
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/react.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >React</p>
+            <p className="text-center text-bold">Python</p>
           </div>
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/nextjs.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >Next.js</p>
+            <p className="text-center text-bold">Django</p>
           </div>
           <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
-            <img src="/images/linux-tux.svg" alt="" className="mb-4 h-20" />
-            <p className="text-center text-bold"
-            >Linux基礎</p>
+            <p className="text-center text-bold">Go</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">GraphQL</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">Git</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">AWS</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">PostgreSQL</p>
+          </div>
+        </div>
+
+        <h2 className="font-bold text-2xl text-center tracking-wider mt-7">
+          独学
+        </h2>
+        <div className="grid grid-cols-2 gap-8 text-xl tracking-wider mt-7 sm:grid-cols-3">
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">Sass</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">Next.js</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">Ruby</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">Ruby on Rails</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">C</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">Java</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">Firebase</p>
+          </div>
+          <div className="bg-white flex flex-col items-center p-7 rounded-3xl shadow-md w-full">
+            <p className="text-center text-bold">MySQL</p>
           </div>
         </div>
       </div>
     </Layout>
-  )
-}
+  );
+};
 
-export default SkillsPage
+export default SkillsPage;
